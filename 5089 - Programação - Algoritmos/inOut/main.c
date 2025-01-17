788ccfd865dfbf7bc5b88edaa02468883ca0c093
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-01-17 12:59:39 +0000

    ideia de como pensar os ex

--- 5089 - Programação - Algoritmos/inOut/main.c
+++ 5089 - Programação - Algoritmos/inOut/main.c
@@ -20,5 +20,37 @@ int main(void) {
     printf("Dia: %d\nmes: %d\nano: %d", dia, mes, ano);
 
 
+
+
+
+
+
+
+// Faça um Programa que peça dois números e imprima a soma.
+/*
+
+//1º pedir um num
+
+// criar uma var para o num
+    int num1;
+//msg para pedir  num
+    printf("Digite um num");
+// ler num
+    scanf("%d", &num1);
+
+
+
+//2º pedir outro num
+
+//3ª somar os 2 num
+
+//4º mostrar na consola o resultado
+
+
+*/
+
+// ex2 2
+
+
     return 0;
 }
