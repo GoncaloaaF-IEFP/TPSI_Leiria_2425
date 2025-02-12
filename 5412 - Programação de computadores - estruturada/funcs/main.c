33041877f3617734afd7cf92d73f15dc133ad1db
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-02-12 10:39:43 +0000

    inro a func return

--- 5412 - Programação de computadores - estruturada/funcs/main.c
+++ 5412 - Programação de computadores - estruturada/funcs/main.c
@@ -16,19 +16,58 @@ void olaMundo2(int ano) {
     printf("Ola Mundo, em %d\n", ano);
 }
 
+
 void countMinToMax(int min, int max) {
 
+    if (min > max) {
+        printf("o contador tem de ser do menor para o maior\n");
+        return void;
+    }
+
+    printf("Comtador: %d a %d\n", min, max);
+    for (int i = min; i <= max; i++) {
+        printf("%d\n", i);
+    }
 
 }
 
+int soma(int a, int b) {
+    int soma = a + b;
+
+    return soma;
+}
+
+int multip(int a, int b) {
+
+}
+
+int div(int a, int b) {
+
+}
+
+int sub(int a, int b) {
+
+}
 
 int main() {
 
+
     olaMundo();
     count0to10();
-    olaMundo2(2025);
+    olaMundo2(2023);
     olaMundo2(2026);
     olaMundo2(2028);;
+
+    printf("\n\n");
+    countMinToMax(20, 10);
+
+    printf("\n\n");
+
+    countMinToMax(10, 20);
+
+    printf("\n\n");
+
+    int resultado = soma(10, 20);
 
 
     return 0;
