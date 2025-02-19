8d2a30457cf5cbb2b0a1f609eca69b2f9a39b301
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-02-19 09:22:29 +0000

    inicio da aula

--- 5412 - Programação de computadores - estruturada/funcs/main.c
+++ 5412 - Programação de computadores - estruturada/funcs/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 void olaMundo() {
@@ -21,7 +22,7 @@ void countMinToMax(int min, int max) {
 
     if (min > max) {
         printf("o contador tem de ser do menor para o maior\n");
-        return void;
+        return;
     }
 
     printf("Comtador: %d a %d\n", min, max);
@@ -38,20 +39,31 @@ int soma(int a, int b) {
 }
 
 int multip(int a, int b) {
-
+    int res = a * b;
+    return res;
 }
 
 int div(int a, int b) {
-
+    int res = a / b;
+    return res;
 }
+
 
 int sub(int a, int b) {
-
+    int res = a - b;
+    return res;
 }
 
+
 int main() {
+    // var que vao ser utilizadas
+    int val1;
+    int val2;
+    int resultado;
+    int dobro;
 
 
+    // Codigo
     olaMundo();
     count0to10();
     olaMundo2(2023);
@@ -67,7 +79,45 @@ int main() {
 
     printf("\n\n");
 
-    int resultado = soma(10, 20);
+    val1 = 20;
+    val2 = 40;
+
+    resultado = soma(val2, val1);
+    printf("o resultado da soma de %d com %d é %d\n",val2, val1, resultado);
+
+    dobro = multip(resultado, 2);
+    printf("o dobro do resultado é: %d\n", dobro);
+
+    system("cls | clear");  // <- Limpa a consola
+
+    val1 = 50;
+    val2 = 40;
+    resultado = soma(val2, val1);
+    printf("o resultado da soma de %d com %d é %d\n",val2, val1, resultado);
+
+
+    system("clear");
+
+    int opt;
+
+    while (1) {
+        printf("opt: ");
+        scanf("%d", &opt);
+
+        if (opt == 1) {
+            printf("soma\n");
+        }
+        if (opt == 2) {
+            printf("Subtração\n");
+        }
+        if (opt == 3) {
+            printf("Multiplicando\n");
+        }
+        if (opt == 4) {
+            printf("Div\n");
+        }
+    }
+
 
 
     return 0;
