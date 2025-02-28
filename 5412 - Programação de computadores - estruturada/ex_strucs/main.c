1bf9aca3f177b90a5f9fc91580345ed6365eebcf
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-02-28 11:39:57 +0000

    ex

---
+++ 5412 - Programação de computadores - estruturada/ex_strucs/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/*
+ * Crie uma aplicação em C que registe carros.
+ *
+ * O número de carros a pedir deve ser pre definido
+ * após adicionar os carros deve lista os mesmos
+ *
+ * Deve usar funções o mais possível
+ *
+ */
+
+
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
