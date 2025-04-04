ea7bef3391418fad59f81766a287c6a2165ef33a
Goncalo Feliciano <goncaloaaf@gmail.com>
2025-04-04 09:18:39 +0100

    fix names

--- 5413 - Programação de computadores - orientada a objetos/11_heranca/desafio.py
+++ 5413 - Programação de computadores - orientada a objetos/11_heranca/desafio.py
@@ -1,6 +1,11 @@
 """
 com base nos conceitos de herança modele:
 
+base Funcionario
+    nome
+    idade
+    num Funcionario
+
 um medico
 
     atributos excluivos  <- exp
@@ -9,7 +14,6 @@ um medico
 
     metodos exclusivos <- exp
         diagnosticar
-
 
 um enfermeiro
     atributos excluivos <- exp
@@ -20,9 +24,68 @@ um enfermeiro
 
 um auxiliar medico
 
+"""
+
+class Funcionario:
+    def __init__(self, nome:str, idade, num_funcionario):
+        self.nome = nome.capitalize()
+        self.idade = idade
+        self.num_funcionario = num_funcionario
 
 
 
+class Medico(Funcionario):
+
+    def __init__(self, nome:str, idade:int, num_funcionario:int, especialidade:str):
+        self.especialidade = especialidade
+        super().__init__(nome, idade, num_funcionario)
+
+    def diagnosticar(self):
+        print(f"medico: {self.nome}, faz um diagnostico")
+
+
+
+
+class Enfermeiro(Funcionario):
+    def __init__(self, nome: str, idade: int, num_funcionario: int, especialista: bool = False):
+        self.especialista = especialista
+        super().__init__(nome, idade, num_funcionario)
+
+
+    def adminstrar_medicamento(self, paciente):
+        print(f"adminstrar medicamento: {paciente}")
+
+
+class Auxiliar(Funcionario):
+    def admitir_paciente(self, paciente):
+        print(f"admitir {paciente}")
+
+
+
+m = Medico("João", 180, 1, "cardiologia")
+
+m.diagnosticar()
+
+
+f = Funcionario("João", 180, 1)
+
+print(f.nome)
+
+
+
+
+"""
+
+modele uma loja. A loja deve ter: funcionarios, responsavies, 1 gerente, e clientes 
+
+classes que identifico
+
+class loja
+class funcionario
+class cliente
+class responsavies
+class gerente 
+class Pessoa
 
 
 """
